2c9de6e844bb4a1793e87c592b56136589e2f3a9
Mohsin Riaz <wilddogers@gmail.com>
2021-05-26 20:13:51 +0200

    initial commit for the app server

---
+++ database/migrations/2021_05_26_174202_create_journals_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJournalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('journals', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->string("date");
+            $table->string("color");
+            $table->float("sentiment_index");
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('journals');
+    }
+}
---
+++ database/migrations/2021_05_26_174221_create_chats_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chats', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('journal_id')->nullable();
+            $table->string("type");
+            $table->longText("body");
+
+            $table->timestamps();
+
+            $table->foreign('journal_id')->references('id')->on('journals');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chats');
+    }
+}
